307bac8ecfceec474eae1032fa0104d651b18c37
Vishwas <baghel.vishwas0@gmail.com>
2022-07-23 13:24:17 +0530

    [Vishwas]add: Creating DataBase For Address Book

---
+++ ADO.NET_AddressBookProgram/AddressBookRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET_AddressBookProgram
+{
+    public class AddressBookRepository
+    {
+        public void CreateAddressBookDataBase()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(@"Data Source=(localdb)\ProjectModels;Initial Catalog=AddressBook_Service;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                connection.Open();
+                SqlCommand command = new SqlCommand("Create DataBase AddressBookAdo", connection);
+                command.ExecuteNonQuery();
+                Console.WriteLine("DataBase For AddressBook Created");
+                connection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
--- ADO.NET_AddressBookProgram/Program.cs
+++ ADO.NET_AddressBookProgram/Program.cs
@@ -6,6 +6,26 @@ namespace ADO.NET_AddressBookProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Adress Book Program With ADO.NET ");
+            AddressBookRepository repository = new AddressBookRepository(); 
+            bool end = true;
+            Console.WriteLine("1. Create DataBase For AddressBook\n2. End The Program");
+            while (true)
+            {
+                Console.WriteLine("Choose an option to execute :");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        repository.CreateAddressBookDataBase();
+                        break;
+                    case 2:
+                        end = false;
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Option");
+                        break;
+                }
+            }
         }
     }
 }
